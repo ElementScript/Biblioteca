4c23bdb799eef180f1acd4dc8ea06ff326a22d13
misterblizzard <ax-filipe@hotmail.com>
2018-06-21 12:51:19 -0500

    Create base

---
+++ system/class/model/DB.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: filipe
+ * Date: 21/06/18
+ * Time: 05:16
+ */
+
+namespace Lib\model;
+
+class DB
+{
+    private $_pdo;
+    public function __construct()
+    {
+        // TODO: create __construct method
+    }
+}
---
+++ system/class/view/Page.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: filipe
+ * Date: 21/06/18
+ * Time: 05:20
+ */
+
+namespace Lib\view;
+
+use Rain\Tpl;
+
+class Page
+{
+    private $_tpl;
+    private $_options = [];
+    private $_defaults = [
+        "header"    => true,
+        "footer"    => true,
+        "data"      => []
+    ];
+
+    public function __construct(array $opts = [], string $route = "/views/")
+    {
+        $this->_defaults["data"]["session"] = $_SESSION;
+
+        $this->_options = array_merge($this->_defaults, $opts);
+
+        $config = [
+            "base_url"  => null,
+            "tpl_dir"   => $_SERVER['DOCUMENT_ROOT'] . $route,
+            "cache_dir" => $_SERVER['DOCUMENT_ROOT'] . "/view-cached/",
+            "debug"     => false
+        ];
+        Tpl::configure( $config );
+        $this->_tpl = new Tpl();
+        if ($this->_options["data"]) $this->setData($this->_options["data"]);
+        if ($this->_options["header"] === true) $this->_tpl->draw("header", false);
+    }
+
+    public function __destruct()
+    {
+        if ($this->_options["footer"] === true) $this->_tpl->draw("footer", false);
+    }
+
+    private function setData($data = [])
+    {
+        foreach ($var as $key => $value)
+        {
+            $this->_tpl->assign($key, $value);
+        }
+    }
+
+    public function setTpl($tplName, $data =[], $returnHTML = false)
+    {
+        $this->setData($data);
+        $this->_tpl->draw($tplName, $returnHTML);
+    }
+}
