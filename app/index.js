5be0468be409a6b6c60f1b23c7ddfb8d5ae996e8
misterblizzard <ax-filipe@hotmail.com>
2018-06-22 18:02:58 -0500

    created header

--- app/index.js
+++ app/index.js
@@ -3,7 +3,10 @@ import Icons from 'uikit/dist/js/uikit-icons';
 import '../src/scss/main.scss';
 import 'jquery';
 
-//UIkit.use(Icons);
+UIkit.use(Icons);
 
-//UIkit.notification({message: '<span uk-icon=\'icon: check\'></span> Message with an icon'}, 'success');
+function reservado() {
+    UIkit.notification({message: '<span uk-icon=\'icon: check\'></span>Reservado com sucesso!'}, 'success');
+}
 
+module.export = reservado;
--- view-cached/header.7d1ab740979cd10ef97e255e2eb1ff35.rtpl.php
+++ view-cached/header.7d1ab740979cd10ef97e255e2eb1ff35.rtpl.php
@@ -4,6 +4,37 @@
     <meta charset="UTF-8">
     <title>Title</title>
     <link rel="stylesheet" href="/dist/main.bundle.css">
+
 </head>
 <body>
-
+    <nav class="uk-navbar-container uk-margin" uk-navbar="mode: click">
+        <div class="uk-navbar-left">
+            <div class="uk-navbar-item uk-link-nav uk-logo">LibMeta <img src="/src/img/logo.png" width="80" alt="logo"></div>
+        </div>
+        <div class="uk-navbar-right">
+            <ul class="uk-navbar-nav">
+                <li>
+                    <a class="uk-link-nav" href="" >Lançamentos<span uk-icon="icon: chevron-down"></span></a>
+                    <div class="uk-navbar-dropdown">
+                        <ul class="uk-nav uk-navbar-dropdown-nav">
+                            <li><a onclick="reservado()" href="#">Reservar</a></li>
+                            <li><a href="#">Emprestar</a></li>
+                            <li><a href="#">Devolver</a></li>
+                        </ul>
+                    </div>
+                </li>
+                <li>
+                    <a class="uk-link-nav" href="">Cadastros<span uk-icon="icon: chevron-down"></span></a>
+                    <div class="uk-navbar-dropdown">
+                        <ul class="uk-nav uk-navbar-dropdown-nav">
+                            <li><a href="#">Livros</a></li>
+                            <li><a href="#">Alunos</a></li>
+                            <li><a href="#">Usuários</a></li>
+                        </ul>
+                    </div>
+                </li>
+                <li><a class="uk-link-nav" href="">Ajuda</a></li>                
+            </ul>
+        </div>
+    </nav>
+    <div class="uk-container">
